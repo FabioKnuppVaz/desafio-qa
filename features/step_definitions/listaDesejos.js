df98971be56dbeecea21c3f9e11d2d68159272da
Fabio V <fabio.knupp@gmail.com>
2022-11-30 13:46:19 -0300

    Update and rename lista_desejos.js to listaDesejos.js

--- features/step_definitions/listaDesejos.js
+++ features/step_definitions/listaDesejos.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 let instance;
 let res;
-let list_id;
+let listId;
 
 Before(function () {
     instance = axios.create(this.parameters);
@@ -24,8 +24,8 @@ Then('validar o resultado da criacao da lista', function (table) {
     esperado = table.hashes()[0]
 
     assert.equal(res.status, 201);
-    assert.equal(res.data.status_code, esperado.status_code);
-    assert.equal(res.data.status_message, esperado.status_message);
+    assert.equal(res.data.status_code, esperado.statusCode);
+    assert.equal(res.data.status_message, esperado.statusMessage);
     assert.equal(res.data.success, true);
     assert.notEqual(res.data.list_id, null);
 });
@@ -33,7 +33,7 @@ Then('validar o resultado da criacao da lista', function (table) {
 Given('realizar requisicao para ver os detalhes da lista', async function () {
     res = await instance({
         method: 'get',
-        url: `/list/${list_id}?language=pt-BR`
+        url: `/list/${listId}?language=pt-BR`
     });
 });
 
@@ -41,23 +41,23 @@ Then('validar o resultado dos detalhes da lista', function (table) {
     esperado = table.hashes()[0]
 
     assert.equal(res.status, 200);
-    assert.equal(res.data.created_by, esperado.created_by);
+    assert.equal(res.data.created_by, esperado.createdBy);
     assert.equal(res.data.description, esperado.description);
     assert.equal(res.data.favorite_count, 0);
     assert.notEqual(res.data.id, null);
     assert.notEqual(res.data.items, null);
     assert.equal(res.data.item_count, 0);
-    assert.equal(res.data.iso_639_1, esperado.iso_639_1);
+    assert.equal(res.data.iso_639_1, esperado.iso6391);
     assert.equal(res.data.name, esperado.name);
     assert.equal(res.data.poster_path, null);
 });
 
-When('realizar requisicao para adicionar filme id {string} na lista', async function (media_id) {
-    data = media_id == "" ? {} : { "media_id": media_id }
+When('realizar requisicao para adicionar filme id {string} na lista', async function (mediaId) {
+    data = mediaId == "" ? {} : { "media_id": mediaId }
     
     res = await instance({
         method: 'post',
-        url: `/list/${list_id}/add_item`,
+        url: `/list/${listId}/add_item`,
         data: data,
         validateStatus: function(status) {
             return true;
@@ -65,16 +65,16 @@ When('realizar requisicao para adicionar filme id {string} na lista', async func
     });
 });
 
-Then('validar o resultado do filme adicionado na lista {string} {string} {string}', function (http, status_code, status_message) {
+Then('validar o resultado do filme adicionado na lista {string} {string} {string}', function (http, statusCode, statusMessage) {
     assert.equal(res.status, http);
-    assert.equal(res.data.status_code, status_code);
-    assert.equal(res.data.status_message, status_message);
+    assert.equal(res.data.status_code, statusCode);
+    assert.equal(res.data.status_message, statusMessage);
 });
 
-When('realizar requisicao para ver o status de um filme id {string} na lista', async function (media_id) {
+When('realizar requisicao para ver o status de um filme id {string} na lista', async function (mediaId) {
     res = await instance({
         method: 'get',
-        url: `/list/${list_id}/item_status?movie_id=${media_id}`,
+        url: `/list/${listId}/item_status?movie_id=${mediaId}`,
         validateStatus: function (status) {
             return true;
         },
@@ -87,12 +87,12 @@ Then('validar o resultado do status de um filme na lista', function () {
     assert.equal(res.data.item_present, true);
 });
 
-Given('realizar requisicao para remover filme id {string} da lista', async function (media_id) {
+Given('realizar requisicao para remover filme id {string} da lista', async function (mediaId) {
     res = await instance({
         method: 'post',
-        url: `/list/${list_id}/remove_item`,
+        url: `/list/${listId}/remove_item`,
         data: {
-            "media_id": parseInt(media_id)
+            "media_id": parseInt(mediaId)
         }
     });
 });
@@ -102,8 +102,8 @@ Then('validar o resultado do filme removido da lista', function (table) {
 
     assert.equal(res.status, 200);
     assert.equal(res.data.success, true);
-    assert.equal(res.data.status_code, esperado.status_code);
-    assert.equal(res.data.status_message, esperado.status_message);
+    assert.equal(res.data.status_code, esperado.statusCode);
+    assert.equal(res.data.status_message, esperado.statusMessage);
 });
 
 When('realizar requisicao para limpar lista', async function () {
@@ -118,14 +118,14 @@ Then('validar o resultado da limpeza da lista', function (table) {
 
     assert.equal(res.status, 201);
     assert.equal(res.data.success, true);
-    assert.equal(res.data.status_code, esperado.status_code);
-    assert.equal(res.data.status_message, esperado.status_message);
+    assert.equal(res.data.status_code, esperado.statusCode);
+    assert.equal(res.data.status_message, esperado.statusMessage);
 });
 
 When('realizar requisicao para deletar uma lista', async function () {
     res = await instance({
         method: 'delete',
-        url: `/list/${list_id}`
+        url: `/list/${listId}`
     })
         .catch(function (error) {
         })
